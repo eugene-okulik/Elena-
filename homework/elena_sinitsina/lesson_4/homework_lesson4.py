2868d7c3e78b8dbcc2f5cfd0792e3767a84eab2d
Elena Sinitsina <sinitsinaelena24@gmail.com>
2024-07-06 23:43:03 +0700

    New file with homework added

--- homework/elena_sinitsina/lesson_4/homework_lesson4.py
+++ homework/elena_sinitsina/lesson_4/homework_lesson4.py
@@ -1,4 +1,4 @@
-my_dict = {'tuple': (3, 100, 4, 78, 8, 4),
+my_dict = {'tuple': (3, 100, 4, 90, 8, 4),
            'list': [4, 678, 900, 'rrr', 777],
            'dict': {'one': 1, 'two': 'two'},
            'set': {1, False, 7, 'text'}}
