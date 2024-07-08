c72554af9b00857b6ea121ef8d31c6eca01369c5
Elena Sinitsina <sinitsinaelena24@gmail.com>
2024-07-08 22:27:36 +0700

    Corrected file with a new key

--- homework/elena_sinitsina/lesson_4/homework_lesson4.py
+++ homework/elena_sinitsina/lesson_4/homework_lesson4.py
@@ -6,8 +6,9 @@ tuple_value = my_dict['tuple']
 print(tuple_value[-1])
 my_dict['list'].append('text')
 my_dict['list'].pop(1)
-tuple_new = (25, 3, 'easylife')
-my_dict['dict']['i am a tuple'] = tuple_new
+tuple_value_new = (1)
+key_new = ('i am a tuple')
+my_dict['dict'][key_new] = tuple_value_new
 popped_value = my_dict['dict'].pop('two')
 my_dict['set'].add('timeflies')
 popped_value_2 = my_dict['set'].pop()
