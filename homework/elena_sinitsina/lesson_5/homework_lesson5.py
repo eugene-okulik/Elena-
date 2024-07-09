a29f5cbe828d48aaf506e6d8b1e522e55155caf4
Elena Sinitsina <sinitsinaelena24@gmail.com>
2024-07-09 22:46:43 +0700

    Corrected mistake with #

--- homework/elena_sinitsina/lesson_5/homework_lesson5.py
+++ homework/elena_sinitsina/lesson_5/homework_lesson5.py
@@ -1,9 +1,9 @@
-#Task_1:
+# Task_1:
 person = ['John', 'Doe', 'New York', '+1372829383739', 'US']
 name, last_name, city, phone, country = person
 print(name, last_name, city, phone, country)
 print(f"name: {name}, last_name: {last_name}, city: {city}, phone: {phone}, country: {country}")
-#Task_2:
+# Task_2:
 string_1 = 'результат операции: 42'
 string_2 = 'результат операции: 514'
 string_3 = 'результат работы программы: 9'
@@ -17,7 +17,7 @@ len_3 = len(string_3)
 index_3 = string_3.index(":") + 2
 number_3 = int(string_3[index_3:len_3]) + 10
 print(number_1, number_2, number_3)
-#Task_3:
+# Task_3:
 students = ['Ivanov', 'Petrov', 'Sidorov']
 student_1, student_2, student_3 = students
 subjects = ['math', 'biology', 'geography']
