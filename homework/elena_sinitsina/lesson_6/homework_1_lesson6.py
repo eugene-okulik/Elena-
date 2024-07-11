4b8ad2eaa4e30ff851885aa4a1e130cd466ccc0c
Elena Sinitsina <sinitsinaelena24@gmail.com>
2024-07-12 01:43:53 +0700

    Added 2 files with homework from lesson_6

---
+++ homework/elena_sinitsina/lesson_6/homework_1_lesson6.py
@@ -0,0 +1,13 @@
+text = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae li'
+words = text.split()
+fin_words = []
+for word in words:
+    if word[-1] in ['.',',']:
+        word_new_p1 = word[:-1]
+        word_new_p2 = word[-1]
+        word_new = word_new_p1 + 'ing' + word_new_p2
+    else: word_new = word + 'ing'
+    fin_words.append(word_new)
+new_text = ' '.join(fin_words)
+print(new_text)
+
---
+++ homework/elena_sinitsina/lesson_6/homework_2_lesson6.py
@@ -0,0 +1,9 @@
+for x in range(1, 101):
+    if x % 3 == 0:
+        print('Fuzz')
+    elif x % 5 == 0:
+        print('Buzz')
+    elif x % 3 == 0 and x % 5 == 0:
+        print('FuzzBuzz')
+    else: print(x)
+
