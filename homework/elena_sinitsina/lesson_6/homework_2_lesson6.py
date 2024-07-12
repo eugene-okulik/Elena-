aed5710643ed77069a816067218b6f82297abccb
Elena Sinitsina <sinitsinaelena24@gmail.com>
2024-07-12 15:37:49 +0700

    The mistake about FuzzBuzz fixed

--- homework/elena_sinitsina/lesson_6/homework_2_lesson6.py
+++ homework/elena_sinitsina/lesson_6/homework_2_lesson6.py
@@ -1,7 +1,7 @@
 for x in range(1, 101):
-    if x % 3 == 0:
+    if x % 3 == 0 and x % 5 != 0:
         print('Fuzz')
-    elif x % 5 == 0:
+    elif x % 5 == 0 and x % 3 != 0:
         print('Buzz')
     elif x % 3 == 0 and x % 5 == 0:
         print('FuzzBuzz')
