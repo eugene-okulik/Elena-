101c648f5bad813ab23c3563f44062d5ddf78436
elena sinitsina <sinitsinaelena36@gmail.com>
2025-05-29 17:42:47 +0700

    Added main.py and Dockerfile

---
+++ main.py
@@ -0,0 +1,17 @@
+import datetime
+
+motivations = {
+    0: "🌞 Monday: A fresh start. Make it count!",
+    1: "💡 Tuesday: Progress over perfection.",
+    2: "💥 Wednesday: You're halfway there!",
+    3: "🌈 Thursday: Small steps = Big results.",
+    4: "🔥 Friday: Finish strong!",
+    5: "😎 Saturday: Recharge and reflect.",
+    6: "🎯 Sunday: Plan, dream, believe."
+}
+
+today = datetime.datetime.today().weekday()
+print("💬 Your motivation for today:")
+print(motivations[today])
+
+
